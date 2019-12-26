4d8a8865a7c1270487cfc2d20ba6df656c8920cb
PowerChuck <53296126+PowerChuck@users.noreply.github.com>
2019-12-26 17:07:13 +0800

    Add files via upload

---
+++ UpdateTagsForResource.ps1
@@ -0,0 +1,20 @@
+﻿$Allinformation = Import-Csv -path C:\temp\Information.csv
+foreach ( $singleinfor in $Allinformation ) 
+{
+$rgname = $singleinfor.ResourceGroup
+$vmname = $singleinfor.ServerName
+$appname = $singleinfor.updatedApplicationName
+$owner = $singleinfor.updatedApplicationOwner
+$envir = $singleinfor.UpdatedEnvironment
+
+
+$tags = (Get-AzResource -ResourceGroupName "$rgname" -Name "$vmname").Tags
+
+
+
+$tags.'af-applicationname' = "$appname"
+$tags.'af-applicationowner' =  "$owner"
+$tags.'af-environment' = "$envir"
+
+Set-AzResource -ResourceGroupName "$rgname" -ResourceName "$vmname" -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force
+}
